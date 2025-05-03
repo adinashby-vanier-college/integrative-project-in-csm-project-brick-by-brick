586af7b102c92c3c69dcca93a0289257827079d7
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2025-05-03 15:22:28 -0400

    test: Add Some CalcLox Tests
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- Deliverable 3/AllInOneCalculator/src/test/java/CalcLoxTest.java
+++ Deliverable 3/AllInOneCalculator/src/test/java/CalcLoxTest.java
@@ -44,4 +44,145 @@ public class CalcLoxTest {
 
         assert false;
     }
+
+    @Test
+    public void testCalculation() {
+        CalculatorFrontendImpl frontend = new CalculatorFrontendImpl();
+
+        String source = """
+                var a = 1;
+                var b = 2;
+                output a + b;
+                """;
+
+        try {
+            CalcLoxRunner.run(frontend, source);
+        } catch (SuccessResult e) {
+            assert e.result != null;
+            assert e.result.equals("3.0");
+            return;
+        }
+
+        assert false;
+    }
+
+    @Test
+    public void testIfElse1() {
+        CalculatorFrontendImpl frontend = new CalculatorFrontendImpl();
+
+        String source = """
+                var a = 1;
+                var b = 2;
+                if (a < b) {
+                    output "a is less than b";
+                } else {
+                    output "a is not less than b";
+                }
+                """;
+
+        try {
+            CalcLoxRunner.run(frontend, source);
+        } catch (SuccessResult e) {
+            assert e.result != null;
+            assert e.result.equals("a is less than b");
+            return;
+        }
+
+        assert false;
+    }
+
+    @Test
+    public void testIfElse2() {
+        CalculatorFrontendImpl frontend = new CalculatorFrontendImpl();
+
+        String source = """
+                var a = 2;
+                var b = 1;
+                if (a < b) {
+                    output "a is less than b";
+                } else {
+                    output "a is not less than b";
+                }
+                """;
+
+        try {
+            CalcLoxRunner.run(frontend, source);
+        } catch (SuccessResult e) {
+            assert e.result != null;
+            assert e.result.equals("a is not less than b");
+            return;
+        }
+
+        assert false;
+    }
+
+    @Test
+    public void testWhileLoop() {
+        CalculatorFrontendImpl frontend = new CalculatorFrontendImpl();
+
+        String source = """
+                var a = 0;
+                while (a < 5) {
+                    a = a + 1;
+                }
+                output a;
+                """;
+
+        try {
+            CalcLoxRunner.run(frontend, source);
+        } catch (SuccessResult e) {
+            assert e.result != null;
+            assert e.result.equals("5.0");
+            return;
+        }
+
+        assert false;
+    }
+
+    @Test
+    public void testForLoop() {
+        CalculatorFrontendImpl frontend = new CalculatorFrontendImpl();
+
+        String source = """
+                var a = 0;
+                for (var i = 0; i < 5; i = i + 1) {
+                    a = a + 1;
+                }
+                output a;
+                """;
+
+        try {
+            CalcLoxRunner.run(frontend, source);
+        } catch (SuccessResult e) {
+            assert e.result != null;
+            assert e.result.equals("5.0");
+            return;
+        }
+
+        assert false;
+    }
+
+    @Test
+    public void testFunction() {
+        CalculatorFrontendImpl frontend = new CalculatorFrontendImpl();
+
+        String source = """
+                fun addOne(a, b) {
+                    return a + b + 1;
+                }
+                
+                var result = addOne(1, 2);
+                output result;
+                """;
+
+        try {
+            CalcLoxRunner.run(frontend, source);
+        } catch (SuccessResult e) {
+            assert e.result != null;
+            assert e.result.equals("4.0");
+            return;
+        }
+
+        assert false;
+    }
 }
